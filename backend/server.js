8b8d0e6b08017f621bf7db204761a4b4607b9fb8
Dipanshuk1 <dipanshukumar1936@gmail.com>
2026-02-04 11:13:33 +0530

    Add MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from "./configs/mongodb.js";
 
+dotenv.config();
 const app = express();
+await connectDB();
 const PORT = process.env.PORT || 4000;
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World! jefnevf');
+  res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
